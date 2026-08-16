64a39cd3ff0f310a37a79b48c3eae7ae33ddfb75
Arnaout <mohammadrami15@yahoo.com>
2026-08-16 10:31:58 +0300

    Fixed using antigravity the mobile view hopefully

--- server/server.js
+++ server/server.js
@@ -24,6 +24,17 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Path normalization for Netlify Serverless Function requests
+app.use((req, res, next) => {
+  if (req.url.startsWith('/.netlify/functions/api')) {
+    req.url = req.url.replace('/.netlify/functions/api', '') || '/';
+  }
+  if (!req.url.startsWith('/api')) {
+    req.url = '/api' + (req.url.startsWith('/') ? req.url : '/' + req.url);
+  }
+  next();
+});
+
 // Serve static assets
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, '../client/dist')));
